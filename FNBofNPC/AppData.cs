077c4a209effb2f91c8152ba6a5b526fdd8c3058
IamNAME <IamNAMEvideos@gmail.com>
2017-03-27 14:07:27 -0500

    Update v0.01
    Added SJ's work on Customer class and main form.

---
+++ FNBofNPC/AppData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNBofNPC
+{
+    abstract class AppData
+    {
+        string _appDirectory = @"C:\myfiles\FNBOfNPC";
+        string _customerListName = "CData.txt";
+        string _accountListName = "AData.txt";
+        string _transactionListName = "TData.txt";
+
+        public string _appDir
+        {
+            get
+            {
+                return _appDir;
+            }
+            set
+            {
+                _appDir = value;
+            }
+        }
+
+        public string _cListName
+        {
+            get
+            {
+                return _cListName;
+            }
+            set
+            {
+                _cListName = value;
+            }
+        }
+
+        public string _aListName
+        {
+            get
+            {
+                return _aListName;
+            }
+            set
+            {
+                _aListName = value;
+            }
+        }
+
+        public string _tListName
+        {
+            get
+            {
+                return _tListName;
+            }
+            set
+            {
+                _tListName = value;
+            }
+        }
+    }
+}
